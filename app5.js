514cd60a779434f136d6981cf1d99e7238b38f57
kohta227 <s24g1107xu@s.chibakoudai.jp>
2024-12-27 22:51:42 +0900

    性格診断とたし算の機能を追加

--- app5.js
+++ app5.js
@@ -59,4 +59,53 @@ app.get("/janken", (req, res) => {
   res.render( 'janken', display );
 });
 
+app.get("/lottely", (req, res) => {
+  const input = req.query.number; 
+  let result = '';
+
+  if (input === "1") {
+    const random = Math.random(); // 0 <= random < 1 のランダム値
+    result = random < 0.2 ? "当たり" : "外れ";
+  } else {
+    result = "1 を入力してください";
+  }
+
+  res.send(`結果: ${result}`);
+});
+
+app.get("/add", (req, res) => {
+  const num1 = Number(req.query.num1); 
+  const num2 = Number(req.query.num2); 
+
+  if (isNaN(num1) || isNaN(num2)) {
+    res.send("Error: num1とnum2には数字を指定してください。");
+    return;
+  }
+
+  const sum = num1 + num2; 
+  res.send(`結果: ${num1} + ${num2} = ${sum}`); 
+});
+
+
+
+
+app.get("/questions", (req, res) => {
+  const answer1 = req.query.answer1; 
+  const answer2 = req.query.answer2; 
+  const answer3 = req.query.answer3; 
+
+  let yesCount = 0;
+  if (answer1 === "はい") yesCount++;
+  if (answer2 === "はい") yesCount++;
+  if (answer3 === "はい") yesCount++;
+
+  let result = "";
+  if (yesCount === 0) result = "陰キy...内向的";
+  else if (yesCount === 1) result = "ちょっと内向的";
+  else if (yesCount === 2) result = "ちょっと外交的";
+  else if (yesCount === 3) result = "陽キy...外交的";
+
+  res.render("questions", { result, answer1, answer2, answer3 });
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
